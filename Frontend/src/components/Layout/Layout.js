0a33f221bbbac5684c5c1eda0b2abc118d644eb9
Tung <krittapop.kms@gmail.com>
2025-03-09 03:01:20 +0700

    feat: navbar toggle

--- Frontend/src/components/Layout/Layout.js
+++ Frontend/src/components/Layout/Layout.js
@@ -1,32 +1,40 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Outlet } from 'react-router-dom';
 import Header from './_components/Header/Header';
 import Footer from './_components/Footer/Footer';
 import NavBar from './_components/NavBar/NavBar';
 
 const Layout = () => {
+  const [isNavbarOpen, setIsNavbarOpen] = useState(false);
+
+  const toggleNavbar = () => {
+    setIsNavbarOpen((prev) => !prev);
+  };
+
   return (
     <div className="flex flex-col min-h-screen">
       {/* Header on the top */}
       <header className="bg-gray-800 text-white px-4 w-full h-20 flex items-center justify-between">
-        <Header />
+        <Header
+          toggleNavbar={toggleNavbar}
+        />
       </header>
 
-
-      {/* NavBar on the left */}
-      <div className="flex flex-grow">
-        {/* NavBar (left side) */}
-        <div className="w-28 bg-gray-300 p-4">
-          <NavBar />
+      {/* Main content area (right side) */}
+      <main className="flex flex-grow py-4 bg-white justify-center relative">
+        <div className="flex flex-col items-center justify-start gap-2">
+          {isNavbarOpen &&
+            <div
+              className="absolute top-0 left-0 flex justify-start items-star p-4 bg-black w-full h-full md:w-auto md:h-auto"
+            >
+              <NavBar
+                toggleNavbar={toggleNavbar}
+              />
+            </div>
+          }
+          <Outlet />
         </div>
-
-        {/* Main content area (right side) */}
-        <main className="flex flex-grow py-4 bg-white justify-center">
-          <div className="flex flex-col items-center justify-start gap-2">
-            <Outlet />
-          </div>
-        </main>
-      </div>
+      </main>
 
       {/* Footer at the bottom */}
       <footer className="bg-gray-800 text-white p-4 w-full h-min">
